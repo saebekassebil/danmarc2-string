227fd986bd958ce42e67072679409886c25148e2
Jakob Miland <saebekassebil@gmail.com>
2014-06-19 11:24:28 +0200

    Add tests

---
+++ test/strings.js
@@ -0,0 +1,15 @@
+var dm2 = require('../');
+var test = require('tape');
+
+test('Simple strings', function(t) {
+  var strings = [
+    '001 00 *a 008681048 *b 810015 *f a',
+    'mail@example.com',
+    'abcdefghjklmnopqrstuvwxyzæøåÆØÅ',
+    'Multi\nline',
+    'Téxte wîth aðmîrable diãcritics',
+  ];
+
+  t.plan(strings.length);
+  strings.forEach(function(str) { t.equal(dm2(str), str); })
+});
