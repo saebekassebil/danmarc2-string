c6264960b297e3ac5fe8ffc94779769f230bec5d
Jakob Miland <saebekassebil@gmail.com>
2014-06-19 11:12:45 +0200

    Initial release

---
+++ index.js
@@ -0,0 +1,32 @@
+module.exports = function(string) {
+  var ch, special = '', parseSpecial = false, res = '', unicodeNum = ''
+
+  for (var i = 0, len = string.length; i < len; i++) {
+    ch = string[i]
+    if (parseSpecial) {
+      if (unicodeNum || !isNaN(parseInt(ch, 16))) {
+        if (unicodeNum.length === 3) {
+          res += String.fromCharCode(parseInt(unicodeNum + ch, 16))
+          unicodeNum = ''
+          parseSpecial = false
+        } else
+          unicodeNum += ch
+
+        continue
+      } else if (ch === '*' || ch === '@' || ch === '¤')
+        res += ch
+      else if  (ch === 'å')
+        res += '\ua733'
+      else if (ch === 'Å')
+        res += '\ua732'
+
+      parseSpecial = false
+    }
+    else if (ch === '@')
+      parseSpecial = true
+    else
+      res += ch
+  }
+
+  return res
+}
