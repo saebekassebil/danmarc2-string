d44af921be14dac5948ba30b2cf5355f8b6f019c
Jakob Miland <saebekassebil@gmail.com>
2014-06-19 11:24:50 +0200

    Use semicolons

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 module.exports = function(string) {
-  var ch, special = '', parseSpecial = false, res = '', unicodeNum = ''
+  var ch, special = '', parseSpecial = false, res = '', unicodeNum = '';
 
   for (var i = 0, len = string.length; i < len; i++) {
-    ch = string[i]
+    ch = string[i];
+
     if (parseSpecial) {
       if (unicodeNum || !isNaN(parseInt(ch, 16))) {
         if (unicodeNum.length === 3) {
-          res += String.fromCharCode(parseInt(unicodeNum + ch, 16))
-          unicodeNum = ''
-          parseSpecial = false
+          res += String.fromCharCode(parseInt(unicodeNum + ch, 16));
+          unicodeNum = '';
+          parseSpecial = false;
         } else
-          unicodeNum += ch
+          unicodeNum += ch;
 
-        continue
+        continue;
       } else if (ch === '*' || ch === '@' || ch === '¤')
-        res += ch
+        res += ch;
       else if  (ch === 'å')
-        res += '\ua733'
+        res += '\ua733';
       else if (ch === 'Å')
-        res += '\ua732'
+        res += '\ua732';
 
-      parseSpecial = false
+      parseSpecial = false;
     }
     else if (ch === '@')
-      parseSpecial = true
+      parseSpecial = true;
     else
-      res += ch
+      res += ch;
   }
 
-  return res
+  return res;
 }
